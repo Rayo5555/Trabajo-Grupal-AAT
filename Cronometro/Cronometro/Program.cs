f4833bcb4415bf2d0df3e4679d893c79a4aea7b6
Rayo5555 <maroperrone@gmail.com>
2025-04-08 16:07:18 -0300

    Cronometro y Semáforo

---
+++ Cronometro/Cronometro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Cronometro
+    {
+        public int minutos = 0;
+        public int segundos = 0;
+        public void Reiniciar()
+        {
+            segundos = 0;
+            minutos = 0;
+        }
+        public void IncrementarTiempo()
+        {
+            segundos++;
+            if ((segundos % 60) == 0)
+            {
+                minutos++;
+                segundos = 0;
+            }
+        }
+        public String MostrarTiempo()
+        {
+            String tiempo = minutos + " Minutos, " + segundos + " Segundos.";
+            return tiempo;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cronometro miCronometro = new Cronometro();
+
+            for (int i = 0; i < 5000; i++)
+            {
+                miCronometro.IncrementarTiempo();
+            }
+            Console.WriteLine(miCronometro.MostrarTiempo());
+            miCronometro.Reiniciar();
+            for (int i = 0; i < 80; i++)
+            {
+                miCronometro.IncrementarTiempo();
+            }
+            Console.WriteLine(miCronometro.MostrarTiempo());
+        }
+    }
+}
---
+++ Semaforo/Semaforo/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Semaforo
+{
+    public class Semaforo
+    {
+        public string color;
+        public int tiempo = 0;
+        public bool intermitente = false;
+
+        public Semaforo(string color = "Rojo")
+        {
+            this.color = color;
+        }
+
+        public void pasoDelTiempo(int s)
+        {
+            tiempo += s;
+            while(((color=="Rojo" && tiempo>29) || (color == "Verde" && tiempo > 19) || ((color == "Rojo-Amarillo" || color =="Amarillo")&& tiempo > 1)) && intermitente == false)
+            {
+                switch (color)
+                {
+                    case "Rojo":
+                        {
+                            tiempo -= 30;
+                            color = "Rojo-Amarillo";
+                            break;
+                        }
+                    case "Verde":
+                        {
+                            tiempo -= 20;
+                            color = "Amarillo";
+                            break;
+                        }
+                    case "Amarillo":
+                        {
+                            tiempo -= 2;
+                            color = "Rojo";
+                            break;
+                        }
+                    default:
+                        {
+                            tiempo -= 2;
+                            color = "Verde";
+                            break;
+                        }
+                }   
+            }
+            if (intermitente)
+            {
+                tiempo = tiempo % 2;
+                if (tiempo == 0)
+                {
+                    color = "Amarillo";
+                }
+                else
+                {
+                    color = "Apagado";
+                }
+            }
+        }
+        public void ponerEnIntermitente()
+        {
+            intermitente = true;
+            color = "Amarillo";
+            tiempo = 0;
+        }
+
+        public void sacarDeIntermitente()
+        {
+            intermitente = false;
+            color = "Amarillo";
+            tiempo = 0;
+        }
+
+        public void mostrarColor()
+        {
+            Console.WriteLine(color);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaforo miSemaforo = new Semaforo();
+            miSemaforo.pasoDelTiempo(24);
+            miSemaforo.pasoDelTiempo(10);
+            miSemaforo.mostrarColor();
+            miSemaforo.ponerEnIntermitente();
+            miSemaforo.pasoDelTiempo(9);
+            miSemaforo.mostrarColor();
+            miSemaforo.sacarDeIntermitente();
+            miSemaforo.pasoDelTiempo(2);
+            miSemaforo.mostrarColor();
+        }
+    }
+}
